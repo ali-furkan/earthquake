c598872249cab9c2f317c87a8b09ad670c9814d9
ali-furkqnm <a.furkan.2003@gmail.com>
2019-10-14 21:28:50 +0300

    fixed

--- handler/socket.js
+++ handler/socket.js
@@ -2,15 +2,18 @@ var io = require("../index").io,
 fs = require("fs"),
 eqData = require("../data.json");
 
+var x = 0
+
 fs.watchFile("./data.json",()=>{
     delete require.cache[require.resolve("../data.json")];
     eqData = require("../data.json");
-    io.emit("NewEq",eqData[0])
+    x++
+    if(x>1) io.emit("newEq",eqData[0]);
 })
 
-var usersN=0
 
 module.exports = () => {
+    var usersN=0
     io.on("connection",(socket)=>{
         usersN++
         console.log(`Yeni kullanıcı giriş yaptı. id: ${socket.id}.\nAktif kullanıcı sayısı: ${usersN}`)
